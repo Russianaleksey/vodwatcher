7b34f5eb6ce202a7f909163e9d98181b51a55b9d
Alex Mamontov <alexmamontovprofessional@gmail.com>
2022-08-17 13:52:07 -0400

    cleaned up:

--- vodwatcher/Program.cs
+++ vodwatcher/Program.cs
@@ -16,32 +16,46 @@ namespace vodwatcher
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
+        private const string _VLC_PATH = @"C:\ProgramFiles\VideoLAN\VLC\vlc.exe";
+        private const string _DEV_URL_PATH = @"https://vod.544146.workers.dev/";
+        private string _VOD_ID = String.Empty;
+        private string _CALL_PATH = String.Empty;
 
         public static void Main(string[] args)
         {
-            MainAsync().GetAwaiter().GetResult();
+            SetCallPath();
+            string vlcUri = GetVlcUri();
+            OpenVlcWithUri(vlcUri);
         } 
 
-        public static async Task<string> MainAsync()
+        public static void SetCallPath() 
         {
             Console.WriteLine("Enter twitch vod id: ");
-            string vodId = Console.ReadLine();
+            _VOD_ID = Console.ReadLine();
+            _CALL_PATH = _DEV_URL_PATH + _VOD_ID;
+        }
 
-            var response = await client.GetByteArrayAsync($"https://vod.544146.workers.dev/{vodId}");
+        public static string GetVlcUri()
+        {
+            var response = await client.GetByteArrayAsync(_CALL_PATH);
             var source = Encoding.GetEncoding("utf-8").GetString(response, 0, response.Length - 1);
             source = WebUtility.HtmlDecode(source);
-            HtmlDocument resultat = new HtmlDocument();
-            resultat.LoadHtml(source);
-            List<HtmlNode> toftitle = resultat.DocumentNode.Descendants().Where
+            HtmlDocument webPageReturned = new HtmlDocument();
+            webPageReturned.LoadHtml(source);
+            List<HtmlNode> anchorNodes = webPageReturned.DocumentNode.Descendants().Where
                 (x => (x.Name == "a")).ToList();
 
-            string fullURL = toftitle[0].InnerText;
+            // a few anchor nodes are returned, just grab the first one
+            string fullURL = anchorNodes[0].InnerText;
+            return fullURL;
+        }
+
+        public static void OpenVlcWithUri(string path) 
+        {
             var VLC = new System.Diagnostics.Process();
-            VLC.StartInfo.FileName = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe";
+            VLC.StartInfo.FileName = _VLC_PATH;
             VLC.StartInfo.Arguments = $"-vvv {fullURL}";
             VLC.Start();
-
-            return "stream opened";
         }
     }
 }
